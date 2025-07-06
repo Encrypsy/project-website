4e4912b20eac7ab3970e3ceeab131cbcd46547c4
Malya <malyamaritza07@gmail.com>
2025-07-06 20:22:58 +0700

    Membuat bagian header website game

--- HTML-CSS-JS-PROJECT/Game-Tebak-Bilangan-Website/numberGuessing.js
+++ HTML-CSS-JS-PROJECT/Game-Tebak-Bilangan-Website/numberGuessing.js
@@ -1,27 +1,27 @@
-const min = 1
-const max = 5
+// const min = 1
+// const max = 5
 
-function TebakAngka(min, max) {
-    let checkpoint = 0
-    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+// function TebakAngka(min, max) {
+//     let checkpoint = 0
+//     let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
 
-    while (checkpoint == 0) {
-        let inputNum = Number(window.prompt(`Masukkan angka tebakkan mu (${min} - ${max}): `))
+//     while (checkpoint == 0) {
+//         let inputNum = Number(window.prompt(`Masukkan angka tebakkan mu (${min} - ${max}): `))
         
-        if (inputNum === randomNum) {
-            window.alert(`YEYY BENER jawabannya ${randomNum}`)
-            checkpoint = 1
-        } else {
-            if (inputNum > randomNum) {
-                window.alert(`LU KELEBIHAN, coba lagi mek`)
-            } else if (inputNum < randomNum) {
-                window.alert(`MASIH KURANG, coba lagi mek`)
-            }
-        }
-    }
-}
+//         if (inputNum === randomNum) {
+//             window.alert(`YEYY BENER jawabannya ${randomNum}`)
+//             checkpoint = 1
+//         } else {
+//             if (inputNum > randomNum) {
+//                 window.alert(`LU KELEBIHAN, coba lagi mek`)
+//             } else if (inputNum < randomNum) {
+//                 window.alert(`MASIH KURANG, coba lagi mek`)
+//             }
+//         }
+//     }
+// }
 
-TebakAngka(1, 5)
+// TebakAngka(1, 5)
 
 
 
